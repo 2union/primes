a06f87f0e0423f76a8a9c4204b5d815ec0d891a2
Daniil Myasnikov <d.myasnikov@skbkontur.ru>
2024-02-29 23:12:50 +0500

    c#

---
+++ csharp.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var primesCount = Enumerable.Range(2, 250001).Aggregate(
+            0, (current, n) => current+(IsPrime(n) ? 1 : 0));
+        Console.WriteLine(primesCount.ToString());
+    }
+
+    private static bool IsPrime(int n)
+    {
+        var a = 2;
+        while (a <= Math.Sqrt(n))
+        {
+            if (n % a < 1)
+                return false;
+            a++;
+        }
+        return (n > 1) ? true : false;
+    }
+}
